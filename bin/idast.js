405b1f97d8384e66a07bcc9c6edfc0d618bc6664
Quinn Slack <qslack@qslack.com>
2013-05-28 02:22:07 -0700

    bin/idast.js utility

---
+++ bin/idast.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+var acorn = require("acorn"), fs = require("fs"), idast = require("../idast"), walk = require("acorn/util/walk");
+
+if (process.argv.length != 3) {
+  console.log("Usage:", process.argv[0], "<input-file>");
+  process.exit(1);
+}
+
+var file = process.argv[2];
+console.error("# AST node IDs in '" + file + "':");
+var i = 0;
+// walk.simple(acorn.parse(fs.readFileSync(file)), {
+//   Statement: function(node, st) {
+//     console.log(st);
+//     i++;
+//   },
+//   Expression: function(node, st) {
+//     console.log(st);
+//     i++;
+//   },
+// }, idast.base, "");
+walk.recursive(acorn.parse(fs.readFileSync(file)), "", {
+  Node: function(node, st, c) {
+    console.log(st, "@", node.start + ":" + node.end);
+  },
+}, idast.base);
+console.error("# Printed " + i + " AST node IDs in '" + file + "'");
